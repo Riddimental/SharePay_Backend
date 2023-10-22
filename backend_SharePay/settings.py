be571a2568c04a7764b75b5cc9ecf3e51a3d59c1
edwin1007 <edwin.leonardo.cuaran@gmail.com>
2023-10-22 14:55:39 -0500

    funciona back sin errores

--- backend_SharePay/settings.py
+++ backend_SharePay/settings.py
@@ -155,7 +155,9 @@ MEDIA_URL= 'media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #CORS auth
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
