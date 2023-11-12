9b6bbd0d264ce97145a11a128046659d5195bde2
Riddimental <juan.villota@correounivalle.edu.co>
2023-11-12 15:39:32 -0500

    eliminando vercel

---
+++ install-script.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+
+
+subprocess.run(['python3', '-m', 'venv', 'venv'])
+
+
+subprocess.run('dir')
+
+
+
+# Instalar las dependencias
+subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
+
