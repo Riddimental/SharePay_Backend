a90730b45fa97e732863179f7ffd2f98d7516ae7
Riddimental <juan.villota@correounivalle.edu.co>
2023-11-12 14:57:51 -0500

    updating scripts

--- run-script.py
+++ run-script.py
@@ -3,8 +3,6 @@ import subprocess
 # activa el entorno virtual
 subprocess.run(['source', 'venv/bin/activate'], shell=True)
 
-# actualiza pip
-subprocess.run(['pip', 'install', '--upgrade', 'pip'])
 
 # ejecuta la aplicacion normalmente
 subprocess.run(['python', 'manage.py', 'runserver'])
--- vercel-script.py
+++ vercel-script.py
@@ -4,8 +4,11 @@ import subprocess
 subprocess.run(['python3', '-m', 'venv', 'venv'])
 subprocess.run(['source', 'venv/bin/activate'], shell=True)
 
+# actualiza pip
+subprocess.run(['pip', 'install', '--upgrade', 'pip'])
+
 # Instalar las dependencias
 subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
 
 # Desactivar el entorno virtual
-subprocess.run(['deactivate'], shell=True)
+subprocess.run(['deactivate'])
