0797eef93ef5922b974fb214bafac8689b58b156
Riddimental <juan.villota@correounivalle.edu.co>
2023-11-12 15:29:05 -0500

    Update vercel-script.py

--- vercel-script.py
+++ vercel-script.py
@@ -7,10 +7,10 @@ subprocess.run(['python3', '-m', 'venv', 'venv'])
 # Activar el entorno virtual
 venv_activate_script = os.path.join('venv', 'bin', 'activate')
 activate_command = f'source {venv_activate_script}'
-subprocess.run(['/bin/bash', '-c', activate_command], shell=True)
+subprocess.run([activate_command], shell=True)
 
 # Actualizar pip
-subprocess.run(['pip', 'install', '--upgrade', 'pip'])
+# subprocess.run(['pip', 'install', '--upgrade', 'pip'])
 
 # Instalar las dependencias
 subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
